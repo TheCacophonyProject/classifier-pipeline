174eac7bce0b7ed9b32da3424ea8abbf4d6eec7d
Clare McLennan <57052+clare@users.noreply.github.com>
2019-05-20 13:00:34 +1200

    For boxes only mode all boxes will now be cyan coloured

--- ml_tools/previewer.py
+++ ml_tools/previewer.py
@@ -129,7 +129,7 @@ class Previewer:
                 image = self.convert_and_resize(thermal, 4.0)
                 draw = ImageDraw.Draw(image)
                 screen_bounds = Region(0, 0, image.width, image.height)
-                self.add_tracks(draw, tracker.tracks, frame_number)
+                self.add_tracks(draw, tracker.tracks, frame_number, colours=[(128, 255, 255)])
 
             if self.preview_type == self.PREVIEW_CLASSIFIED:
                 image = self.convert_and_resize(thermal, 4.0)
@@ -216,8 +216,10 @@ class Previewer:
             draw.rectangle(self.rect_points(rect, v_offset), outline=(128, 128, 128))
 
     def add_tracks(
-        self, draw, tracks, frame_number, track_predictions=None, screen_bounds=None
+        self, draw, tracks, frame_number, track_predictions=None, screen_bounds=None, colours=None
     ):
+        if not colours:
+            colours = self.TRACK_COLOURS
         # look for any tracks that occur on this frame
         for index, track in enumerate(tracks):
             frame_offset = frame_number - track.start_frame
@@ -225,7 +227,7 @@ class Previewer:
                 rect = track.bounds_history[frame_offset]
                 draw.rectangle(
                     self.rect_points(rect),
-                    outline=self.TRACK_COLOURS[index % len(self.TRACK_COLOURS)],
+                    outline=colours[index % len(colours)],
                 )
                 if track_predictions:
                     self.add_class_results(
