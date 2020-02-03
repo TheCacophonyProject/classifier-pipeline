7c10a4d4d41b96abcd180fe531f6c144c87b7786
GP <g.ferraro22@gmail.com>
2020-02-03 16:16:34 +1300

    removed conflict

--- motiontest.py
+++ motiontest.py
@@ -6,10 +6,7 @@ from config.config import Config
 from ml_tools.logs import init_logging
 from piclassifier.motiondetector import MotionDetector
 from config.thermalconfig import ThermalConfig
-<<<<<<< HEAD
-=======
 from .headerinfo import HeaderInfo
->>>>>>> origin/master
 
 
 def parse_args():
@@ -30,18 +27,7 @@ def main():
 
     config = Config.load_from_file(args.config_file)
     thermal_config = ThermalConfig.load_from_file(args.thermal_config_file)
-<<<<<<< HEAD
-    res_x = config.res_x
-    res_y = config.res_y
     print("detecting on  " + args.cptv)
-    motion_detector = MotionDetector(
-        res_x, res_y, thermal_config, config.tracking.dynamic_thresh, None
-    )
-=======
-
-    print("detecting on  " + args.cptv)
-
->>>>>>> origin/master
     with open(args.cptv, "rb") as f:
         reader = CPTVReader(f)
 
