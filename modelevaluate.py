314dd2c1607945bee098f5ab5e0a5a93da1294d9
Giampaolo <g.ferraro22@gmail.com>
2020-09-01 13:43:39 +1200

    all tracks

--- modelevaluate.py
+++ modelevaluate.py
@@ -223,7 +223,7 @@ class ModelEvalute:
             )
             processes.append(p)
             p.start()
-        for track in dataset.tracks[:1]:
+        for track in dataset.tracks:
             job_queue.put(track)
         for i in range(len(processes)):
             job_queue.put("DONE")
