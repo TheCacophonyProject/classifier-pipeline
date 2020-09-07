99b2b610569159997f7fc9358495680a26aad0db
gferraro <g.ferraro22@gmail.com>
2020-09-08 09:58:56 +1200

    eval

--- ml_tools/kerasmodel.py
+++ ml_tools/kerasmodel.py
@@ -31,9 +31,7 @@ HP_DENSE_SIZES = hp.HParam(
 
 HP_BATCH_SIZE = hp.HParam("batch_size", hp.Discrete([16, 32, 64]))
 HP_OPTIMIZER = hp.HParam("optimizer", hp.Discrete(["adam"]))
-HP_LEARNING_RATE = hp.HParam(
-    "learning_rate", hp.Discrete([0.0001, 0.001, 0.01, 0.1, 1.0])
-)
+HP_LEARNING_RATE = hp.HParam("learning_rate", hp.Discrete([0.0001, 0.001, 0.01]))
 
 METRIC_ACCURACY = "accuracy"
 
@@ -667,7 +665,6 @@ class KerasModel:
         dense_size = hparams[HP_DENSE_SIZES].split()
         for i, size in enumerate(dense_size):
             dense_size[i] = int(size)
-        self.build_model(dense_sizes=dense_size)
         self.train = DataGenerator(
             self.datasets.train,
             self.datasets.train.labels,
@@ -702,6 +699,8 @@ class KerasModel:
             type=self.type,
             cap_at="wallaby",
         )
+        self.square_width = self.validate.square_width
+        self.build_model(dense_sizes=dense_size)
 
         opt = None
         learning_rate = hparams[HP_LEARNING_RATE]
