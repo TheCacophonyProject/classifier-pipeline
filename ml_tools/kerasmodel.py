e47b6569fd481ce3b3c2c2c6b81cbc1bb411902d
gferraro <g.ferraro22@gmail.com>
2020-09-23 14:45:35 +1200

    test type 10 and 12

--- ml_tools/kerasmodel.py
+++ ml_tools/kerasmodel.py
@@ -25,17 +25,15 @@ from classify.trackprediction import TrackPrediction
 from sklearn.metrics import confusion_matrix
 
 #
-HP_DENSE_SIZES = hp.HParam(
-    "dense_sizes", hp.Discrete(["1024 512", "1024 1024 1024 512"]),
-)
-HP_TYPE = hp.HParam("type", hp.Discrete([12]))
+HP_DENSE_SIZES = hp.HParam("dense_sizes", hp.Discrete(["1024 512"]),)
+HP_TYPE = hp.HParam("type", hp.Discrete([10, 12]))
 
 HP_BATCH_SIZE = hp.HParam("batch_size", hp.Discrete([32]))
 HP_OPTIMIZER = hp.HParam("optimizer", hp.Discrete(["adam"]))
-HP_LEARNING_RATE = hp.HParam("learning_rate", hp.Discrete([0.001, 0.0001]))
+HP_LEARNING_RATE = hp.HParam("learning_rate", hp.Discrete([0.0001]))
 HP_EPSILON = hp.HParam("epislon", hp.Discrete([1e-7]))  # 1.0 and 0.1 for inception
 HP_RETRAIN = hp.HParam("retrain_layer", hp.Discrete([-1]))
-HP_DROPOUT = hp.HParam("dropout", hp.Discrete([0.3, 0.1, 0.0]))
+HP_DROPOUT = hp.HParam("dropout", hp.Discrete([0.2, 0.1, 0.0]))
 
 METRIC_ACCURACY = "accuracy"
 METRIC_LOSS = "loss"
