3f7e736f4c07dd408715b05dba2d01ee4cb16a36
gferraro <g.ferraro22@gmail.com>
2021-06-11 12:01:58 +1200

    repeats default = 1

--- ml_tools/datasetstructures.py
+++ ml_tools/datasetstructures.py
@@ -270,7 +270,7 @@ class TrackHeader:
         scale=1,
         top_frames=False,
         random_sections=False,
-        repeats=10,
+        repeats=1,
     ):
         self.segments = []
         self.filtered_stats = {"segment_mass": 0}
