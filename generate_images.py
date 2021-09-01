e955efe813e722d83779826ada3faca525a24348
gferraro <g.ferraro22@gmail.com>
2021-09-01 13:25:12 +1200

    add missing scripts

---
+++ generate_images.py
@@ -0,0 +1,137 @@
+# Script to extra images from datasets with meta_data #
+# This can be used to give to people who dont want to work with the h5py db
+from multiprocessing import Process, Queue
+from PIL import Image
+import numpy as np
+import pickle
+import argparse
+import logging
+import os
+import sys
+from config.config import Config
+from ml_tools.imageprocessing import normalize
+import pickle
+
+
+def save_job(queue, dataset, folder, labels_dir):
+
+    i = 0
+    while True:
+        i += 1
+        track = queue.get()
+        if track == "DONE":
+            break
+        else:
+            save_track(dataset, track, folder, labels_dir)
+        if i % 50 == 0:
+            logging.info("%s jobs left", queue.qsize())
+
+
+def save_track(dataset, track, folder, labels_dir, ext="png"):
+    track_data = dataset.fetch_track(track, original=True)
+    clip_meta = dataset.db.get_clip_meta(track.clip_id)
+
+    clip_dir = os.path.join(folder, str(track.clip_id))
+    if not os.path.isdir(clip_dir):
+        logging.debug("Creating %s", clip_dir)
+        os.mkdir(clip_dir)
+
+    background = dataset.db.get_clip_background(track.clip_id)
+    if background is not None:
+        background, _ = normalize(background, new_max=255)
+
+        img = Image.fromarray(np.uint8(background))
+        filename = "{}-background.{}".format(track.clip_id, ext)
+        logging.debug("Saving %s", os.path.join(clip_dir, filename))
+        img.save(os.path.join(clip_dir, filename))
+
+    for i, frame in enumerate(track_data):
+        thermal = frame.thermal
+        normed, _ = normalize(thermal, new_max=255)
+        img = Image.fromarray(np.uint8(normed))
+        filename = "{}-{}.{}".format(track.clip_id, i + track.start_frame, ext)
+        logging.debug("Saving %s", os.path.join(clip_dir, filename))
+
+        img.save(os.path.join(clip_dir, filename))
+    save_metadata(clip_meta, track, labels_dir)
+
+
+def save_metadata(clip_meta, track, folder):
+    meta_dir = os.path.join(folder, track.label)
+    if not os.path.isdir(meta_dir):
+        logging.debug("Creating %s", meta_dir)
+        os.mkdir(meta_dir)
+    filename = "{}-{}.txt".format(track.clip_id, track.track_id)
+    fullpath = os.path.join(meta_dir, filename)
+    with open(fullpath, "w") as f:
+        f.write(track.toJSON(clip_meta))
+
+
+def save_all(dataset, worker_threads, folder):
+    if not os.path.isdir(folder):
+        logging.debug("Creating %s", folder)
+        os.mkdir(folder)
+    labels_dir = os.path.join(folder + "-labels")
+    if not os.path.isdir(labels_dir):
+        logging.debug("Creating %s", labels_dir)
+        os.mkdir(labels_dir)
+    job_queue = Queue()
+    processes = []
+    for i in range(max(1, 4)):
+        p = Process(
+            target=save_job,
+            args=(job_queue, dataset, folder, labels_dir),
+        )
+        processes.append(p)
+        p.start()
+
+    for track in dataset.tracks:
+        job_queue.put(track)
+
+    logging.debug("Processing %d", len(dataset.tracks))
+    for i in range(len(processes)):
+        job_queue.put("DONE")
+    for process in processes:
+        process.join()
+
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--base-folder", help="Base folder to save, train test and val"
+    )
+
+    parser.add_argument("-c", "--config-file", help="Path to config file to use")
+    args = parser.parse_args()
+    return args
+
+
+def init_logging(timestamps=False):
+    """Set up logging for use by various classifier pipeline scripts.
+
+    Logs will go to stderr.
+    """
+
+    fmt = "%(levelname)7s %(message)s"
+    if timestamps:
+        fmt = "%(asctime)s " + fmt
+    logging.basicConfig(
+        stream=sys.stderr, level=logging.INFO, format=fmt, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+args = load_args()
+init_logging()
+config = Config.load_from_file(args.config_file)
+datasets = ["train", "validation", "test"]
+
+base_dir = "."
+if args.base_folder:
+    base_dir = args.base_folder
+if not os.path.isdir(base_dir):
+    logging.debug("Creating %s", base_dir)
+    os.mkdir(base_dir)
+for i, name in enumerate(datasets):
+    dataset = pickle.load(open(f"{os.path.join(config.tracks_folder, name)}.dat", "rb"))
+    dataset.load_db()
+    save_all(dataset, config.worker_threads, os.path.join(base_dir, dataset.name))
---
+++ modelevaluate.py
@@ -0,0 +1,157 @@
+"""
+
+Author Giampaolo Ferraro
+
+Date December 2020
+
+Some tools to evaluate a model
+
+"""
+import argparse
+import logging
+import pickle
+import sys
+import os
+import json
+import pickle
+from config.config import Config
+from ml_tools.kerasmodel import KerasModel
+from ml_tools import tools
+from ml_tools.trackdatabase import TrackDatabase
+
+
+def evaluate_db_clip(model, db, classifier, clip_id, track_id=None):
+    logging.info("Prediction tracks %s", track)
+    clip_meta = db.get_clip_meta(clip_id)
+    if track_id is None:
+        tracks = db.get_clip_tracks(clip_id)
+    else:
+        tracks = [track_id]
+    for track_id in tracks:
+        track_meta = db.get_track_meta(clip_id, track_id)
+        track_data = db.get_track(clip_id, track_id)
+
+        regions = []
+        medians = clip_meta["frame_temp_median"][
+            track_meta["start_frame"] : track_meta["start_frame"] + track_meta["frames"]
+        ]
+        for region in track_meta.track_bounds:
+            regions.append(tools.Rectangle.from_ltrb(*region))
+        track_prediction = model.classify_track_data(
+            track_id, track_data, medians, regions=regions
+        )
+        logging.info("Predicted %s", track_prediction.predicted_tag(model.labels))
+
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model-file",
+        help="Path to model file to use, will override config model",
+    )
+    parser.add_argument("-w", "--weights", help="Weights to load into model")
+
+    parser.add_argument(
+        "-s",
+        "--dataset",
+        default="test.dat",
+        help="Dataset to use train.dat, validation.dat, test.dat ( Default)",
+    )
+    parser.add_argument(
+        "--confusion",
+        help="Confusion matrix filename, used if you want to save confusion matrix image",
+    )
+    parser.add_argument(
+        "--tracks", action="count", help="Evaluate whole track rather than samples"
+    )
+    parser.add_argument("-c", "--config-file", help="Path to config file to use")
+
+    parser.add_argument("--track-id", help="Track id to evaluate from database")
+
+    parser.add_argument("--clip-id", help="Clip id to evaluate from database")
+
+    args = parser.parse_args()
+    return args
+
+
+def init_logging(timestamps=False):
+    """Set up logging for use by various classifier pipeline scripts.
+
+    Logs will go to stderr.
+    """
+
+    fmt = "%(levelname)7s %(message)s"
+    if timestamps:
+        fmt = "%(asctime)s " + fmt
+    logging.basicConfig(
+        stream=sys.stderr, level=logging.INFO, format=fmt, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+args = load_args()
+init_logging()
+config = Config.load_from_file(args.config_file)
+
+
+if args.model_file:
+    model_file = args.model_file
+    weights = args.weights
+else:
+    model_file = config.classify.models[0].model_file
+    weights = config.classify.models[0].model_weights
+
+base_dir = config.tracks_folder
+
+
+model = KerasModel(train_config=config.train)
+model.load_model(model_file, training=False, weights=weights)
+
+if args.track_id or args.clip_id:
+    if args.clip_id is None:
+        logging.error("Need clip id and track id")
+        sys.exit(0)
+    db = TrackDatabase(os.path.join(config.tracks_folder, "dataset.hdf5"))
+    evaluate_db_clip(model, db, args.clip_id, args.track_id)
+    sys.exit(0)
+
+dataset = pickle.load(open(os.path.join(base_dir, args.dataset), "rb"))
+logging.info("running on %s ", dataset.name)
+dataset.recalculate_segments(segment_type=1)
+
+dir = os.path.dirname(model_file)
+meta = json.load(open(os.path.join(dir, "metadata.txt"), "r"))
+mapped_labels = meta.get("mapped_labels")
+label_probabilities = meta.get("label_probabilities")
+dataset.lbl_p = label_probabilities
+if mapped_labels:
+    dataset.regroup(mapped_labels)
+print("dataset labels arenow ", dataset.labels)
+
+logging.info(
+    "Dataset loaded %s, using labels %s, mapped labels %s",
+    dataset.name,
+    dataset.labels,
+    dataset.label_mapping,
+)
+logging.info("%s %s / %s / %s", "label", "segments", "frames", "tracks")
+for label in dataset.labels:
+    segments, frames, tracks, _, _ = dataset.get_counts(label)
+    logging.info("%s %s / %s / %s", label, segments, frames, tracks)
+
+logging.info("Mapped labels")
+for label in dataset.label_mapping.keys():
+    logging.info(
+        "%s",
+        "{} {:<20} {:<20}".format(
+            label,
+            dataset.mapped_label(label),
+            "{}/{}/{}/{:.1f}".format(*dataset.get_counts(label)),
+        ),
+    )
+if args.tracks:
+    model.track_accuracy(dataset, args.confusion)
+elif args.confusion:
+    model.confusion(dataset, args.confusion)
+else:
+    model.evaluate(dataset)
