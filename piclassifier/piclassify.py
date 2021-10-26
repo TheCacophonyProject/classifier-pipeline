477293a5971b0b335da411621d02c9b068295b2c
gferraro <g.ferraro22@gmail.com>
2021-10-27 12:02:05 +1300

    use datetime

--- piclassifier/piclassify.py
+++ piclassifier/piclassify.py
@@ -173,7 +173,7 @@ def handle_connection(connection, config, thermal_config):
                 pass
             read += 1
             frame = raw_frame.parse(data)
-            frame.received_at = time.time()
+            frame.received_at = datetime.now.strftime("%H:%M:%S")
             cropped_frame = crop_rectangle.subimage(frame.pix)
             t_max = np.amax(cropped_frame)
             t_min = np.amin(cropped_frame)
