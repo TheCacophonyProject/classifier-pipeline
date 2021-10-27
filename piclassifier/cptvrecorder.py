08ca510d9315afb661210c27cf699bc2d60c4463
gferraro <g.ferraro22@gmail.com>
2021-10-28 09:49:06 +1300

    encode

--- piclassifier/cptvrecorder.py
+++ piclassifier/cptvrecorder.py
@@ -77,9 +77,9 @@ class CPTVRecorder(Recorder):
         self.motion.temp_thresh = temp_thresh
         self.writer.motion_config = yaml.dump(self.motion.as_dict()).encode()[:255]
         self.motion.temp_thresh = default_thresh
-        self.writer.model = self.headers.model
-        self.writer.brand = self.headers.brand
-        self.writer.firmware = self.headers.firmware
+        self.writer.model = self.headers.model.encode()
+        self.writer.brand = self.headers.brand.encode()
+        self.writer.firmware = self.headers.firmware.encode()
         self.writer.camera_serial = self.headers.serial
         f = Frame(background_frame, timedelta(), timedelta(), 0, 0)
 
