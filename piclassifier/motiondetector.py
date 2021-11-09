314a1e7f0db4ec3ed151ac8c7e0ef3f3990bb725
gferraro <g.ferraro22@gmail.com>
2021-11-10 09:40:29 +1300

    compare with oldest frame after ffc event

--- piclassifier/motiondetector.py
+++ piclassifier/motiondetector.py
@@ -18,13 +18,21 @@ class SlidingWindow:
         self.last_index = None
         self.size = len(self.frames)
         self.oldest_index = None
+        self.non_ffc_index = None
+        self.ffc = False
 
-    def update_current_frame(self, frame):
+    def update_current_frame(self, frame, ffc):
         with self.lock:
             if self.last_index is None:
                 self.oldest_index = 0
                 self.last_index = 0
+                if not ffc:
+                    self.non_ffc_index = self.oldest_index
+            if not ffc and self.ffc:
+                self.non_ffc_index = self.last_index
+
             self.frames[self.last_index] = frame
+            self.ffc = ffc
 
     @property
     def current(self):
@@ -51,24 +59,38 @@ class SlidingWindow:
             return self.frames[i]
 
     @property
+    def oldest_nonffc(self):
+        with self.lock:
+            if self.non_ffc_index is not None:
+                return self.frames[self.non_ffc_index]
+            return None
+
+    @property
     def oldest(self):
         with self.lock:
             if self.oldest_index is not None:
                 return self.frames[self.oldest_index]
             return None
 
-    def add(self, frame):
+    def add(self, frame, ffc):
         with self.lock:
             if self.last_index is None:
                 self.oldest_index = 0
                 self.frames[0] = frame
                 self.last_index = 0
+                if not ffc:
+                    self.non_ffc_index = self.oldest_index
             else:
                 new_index = (self.last_index + 1) % self.size
                 if new_index == self.oldest_index:
+                    if self.oldest_index == self.non_ffc_index and not ffc:
+                        self.non_ffc_index = (self.oldest_index + 1) % self.size
                     self.oldest_index = (self.oldest_index + 1) % self.size
                 self.frames[new_index] = frame
                 self.last_index = new_index
+            if not ffc and self.ffc:
+                self.non_ffc_index = self.last_index
+            self.ffc = ffc
 
     def reset(self):
         with self.lock:
@@ -91,14 +113,12 @@ class MotionDetector:
         edge = self.config.edge_pixels
         self.min_frames = thermal_config.recorder.min_secs * headers.fps
         self.max_frames = thermal_config.recorder.max_secs * headers.fps
-        # self.clipped_window = SlidingWindow(
-        #     (self.compare_gap, headers.res_y - edge * 2, headers.res_x - edge * 2),
-        #     np.int32,
-        # )
-        self.diff_window = SlidingWindow(
-            (self.compare_gap, headers.res_y - edge * 2, headers.res_x - edge * 2),
-            np.int32,
-        )
+
+        if not self.config.one_diff_only:
+            self.diff_window = SlidingWindow(
+                (self.compare_gap, headers.res_y - edge * 2, headers.res_x - edge * 2),
+                np.int32,
+            )
 
         self.thermal_window = SlidingWindow(
             (self.preview_frames, headers.res_y, headers.res_x), "O"
@@ -173,7 +193,7 @@ class MotionDetector:
             self.temp_thresh = self.config.temp_thresh
 
     def detect(self, clipped_frame, received_at=None):
-        oldest = self.crop_rectangle.subimage(self.thermal_window.oldest.pix)
+        oldest = self.crop_rectangle.subimage(self.thermal_window.oldest_nonffc.pix)
         oldest = np.clip(oldest, a_min=self.temp_thresh, a_max=None)
         clipped_frame = np.clip(clipped_frame, a_min=self.temp_thresh, a_max=None)
         delta_frame = clipped_frame - oldest
@@ -183,7 +203,7 @@ class MotionDetector:
             diff = len(delta_frame[delta_frame > self.config.delta_thresh])
         else:
             if self.processed > 2:
-                delta_frame2 = self.diff_window.oldest
+                delta_frame2 = self.diff_window.oldest_nonffc
                 delta_frame[
                     delta_frame >= self.config.delta_thresh
                 ] = self.config.delta_thresh
@@ -197,7 +217,7 @@ class MotionDetector:
                 ] = self.config.delta_thresh
                 diff = 0
 
-        self.diff_window.add(delta_frame)
+            self.diff_window.add(delta_frame, self.ffc_affected)
         if diff > self.config.count_thresh:
             if not self.movement_detected:
                 logging.debug(
@@ -227,30 +247,30 @@ class MotionDetector:
         return self.rec_window.inside_window()
 
     def disconnected(self):
-        # self.clipped_window.reset()
         self.thermal_window.reset()
-        self.diff_window.reset()
+        if not self.config.one_diff_only:
+            self.diff_window.reset()
         self.processed = 0
 
     def process_frame(self, cptv_frame, force_process=False):
+        prev_ffc = self.ffc_affected
+        self.ffc_affected = is_affected_by_ffc(cptv_frame)
         if self.can_record() or force_process:
-            self.thermal_window.add(cptv_frame)
+            self.thermal_window.add(cptv_frame, self.ffc_affected)
+
             cropped_frame = np.int32(self.crop_rectangle.subimage(cptv_frame.pix))
-            prev_ffc = self.ffc_affected
-            self.ffc_affected = is_affected_by_ffc(cptv_frame)
             if not self.ffc_affected:
                 if self.background is None:
                     self.background = cptv_frame.pix
                     self.last_background_change = self.processed
                 else:
                     self.calc_temp_thresh(cropped_frame)
-            # clipped_frame = np.clip(cropped_frame, a_min=self.temp_thresh, a_max=None)
-            # self.clipped_window.add(clipped_frame)
-
             if self.ffc_affected or prev_ffc:
                 logging.debug("{} MotionDetector FFC".format(self.num_frames))
                 self.movement_detected = False
-                # self.clipped_window.oldest_index = self.clipped_window.last_index
+                self.triggered = 0
+                if prev_ffc:
+                    self.thermal_window.non_ffc_index = self.thermal_window.last_index
             elif self.processed > self.detect_after:
                 movement = self.detect(cropped_frame)
                 if movement:
@@ -263,7 +283,7 @@ class MotionDetector:
                     self.movement_detected = False
             self.processed += 1
         else:
-            self.thermal_window.update_current_frame(cptv_frame)
+            self.thermal_window.update_current_frame(cptv_frame, self.ffc_affected)
             self.movement_detected = False
 
         self.num_frames += 1
