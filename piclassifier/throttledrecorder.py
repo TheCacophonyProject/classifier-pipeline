5e59d4afc124666fc72279c969c26a7721d1ddcd
gferraro <g.ferraro22@gmail.com>
2021-11-11 10:36:26 +1300

    debug

--- piclassifier/throttledrecorder.py
+++ piclassifier/throttledrecorder.py
@@ -33,11 +33,17 @@ class ThrottledRecorder(Recorder):
     def process_frame(self, movement_detected, cptv_frame):
         if movement_detected:
             self.last_motion = cptv_frame.received_at
+            logging.info(
+                "Update last_motion%s", datetime.fromtimestamp(self.last_motion)
+            )
+
         was_recording = self.recorder.recording
         self.recorder.process_frame(movement_detected, cptv_frame)
         self.take_token(cptv_frame.received_at)
         if was_recording and not self.recorder.recording:
             self.last_rec = cptv_frame.received_at
+            logging.info("Update last_rec%s", datetime.fromtimestamp(self.last_rec))
+
         if self.throttling and self.recorder.recording:
             logging.info("Throttling recording")
             self.stop_recording()
@@ -49,7 +55,12 @@ class ThrottledRecorder(Recorder):
         update_from = self.last_motion
         if self.last_rec and self.last_rec > self.last_motion:
             update_from = self.last_rec
-
+            logging.info("Using last rec")
+        logging.info(
+            "update_tokens last_rec%s frame %s",
+            datetime.fromtimestamp(update_from),
+            datetime.fromtimestamp(frame_time),
+        )
         since_motion = frame_time - update_from
         # if we have been throttled wait for no motion before adding any tokens back
         if self.throttling:
@@ -90,6 +101,8 @@ class ThrottledRecorder(Recorder):
         logging.debug("Attempting rec have %s tokens", self.tokens)
         self.update_tokens(frame_time)
         self.last_motion = frame_time
+        logging.info("Update last_motion%s", datetime.fromtimestamp(self.last_motion))
+
         if self.throttling or self.tokens < self.min_recording:
             return False
         self.recorder.start_recording(
