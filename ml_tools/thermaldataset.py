b4a3a89c065deb288c856a236ae3f65b1c362bb6
gferraro <g.ferraro22@gmail.com>
2022-04-07 11:13:20 +1200

    combine insect and fp

--- ml_tools/thermaldataset.py
+++ ml_tools/thermaldataset.py
@@ -15,6 +15,9 @@ AUTOTUNE = tf.data.AUTOTUNE
 # IMAGE_SIZE = [256, 256]
 # BATCH_SIZE = 64
 
+insect = None
+fp = None
+
 
 def load_dataset(
     filenames, image_size, num_labels, deterministic=False, labeled=True, augment=False
@@ -57,13 +60,20 @@ def get_dataset(
     filenames,
     batch_size,
     image_size,
-    num_labels,
+    labels,
     reshuffle=True,
     deterministic=False,
     labeled=True,
     resample=True,
     augment=False,
 ):
+    num_labels = len(labels)
+    global insect
+    global fp
+    if "false-positive" in labels:
+        fp = tf.constant(labels.index("false-positive"), tf.int64)
+    if "insect" in labels:
+        insect = tf.constant(labels.index("insect"), tf.int64)
     dataset = load_dataset(
         filenames, image_size, num_labels, deterministic=deterministic, labeled=labeled
     )
@@ -78,7 +88,10 @@ def get_dataset(
         target_dist = np.empty((num_labels), dtype=np.float32)
         for i in range(num_labels):
             dist[i] = c[i]
-            target_dist[i] = 1 / num_labels
+        zeros = dist[dist == 0]
+        non_zero_labels = num_labels - len(zeros)
+        target_dist[:] = 1 / non_zero_labels
+
         print("Initial counts are", dist)
         min_label = np.min(dist)
         dist = dist / np.sum(dist)
@@ -87,9 +100,15 @@ def get_dataset(
         dist_max = np.max(dist)
         dist_min = np.min(dist)
         for i in range(num_labels):
+            if dist[i] == 0:
+                target_dist[i] = 0
             if dist[i] - dist_min > max_range:
+                add_on = max_range
+                if dist[i] - dist_min > max_range * 2:
+                    add_on *= 2
+
                 print("too far above", i)
-                target_dist[i] += max_range
+                target_dist[i] += add_on
 
                 print(
                     "add to taget dist and subtract form init dist",
@@ -97,7 +116,7 @@ def get_dataset(
                     "from label",
                     i,
                 )
-                # dist[i] -= max_range / 2.0
+                dist[i] -= add_on
             elif dist_max - dist[i] > max_range:
                 print("too far below", i)
                 print(
@@ -148,7 +167,11 @@ def read_tfrecord(example, image_size, num_labels, labeled, augment=False):
         #     tf.reshape(example["image/object/class/label"], shape=[]), dtype=tf.int64
         # )
         label = tf.cast(example["image/class/label"], tf.int64)
-
+        global insect
+        global fp
+        if insect is not None and fp is not None:
+            if tf.math.equal(label, insect):
+                label = fp
         onehot_label = tf.one_hot(label, num_labels)
 
         return image, onehot_label
@@ -196,7 +219,7 @@ def main():
     with open(file, "r") as f:
         meta = json.load(f)
     labels = meta.get("labels", [])
-    dataset = get_dataset(train_files, 32, (256, 256), len(labels))
+    dataset = get_dataset(train_files, 32, (256, 256), labels)
 
     print("labels are", labels)
 
@@ -213,12 +236,11 @@ def main():
     for e in range(1):
         print("epoch", e)
         for x, y in dataset:
-            show_batch(x, y)
+            show_batch(x, y, labels)
 
 
-def show_batch(image_batch, label_batch):
+def show_batch(image_batch, label_batch, labels):
     plt.figure(figsize=(10, 10))
-    labels = ["cat", "false-positive", "hedgehog", "possum"]
     print("images in batch", len(image_batch))
     num_images = min(len(image_batch), 25)
     for n in range(num_images):
