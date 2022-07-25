24440b22dffdddef1e3b2a335018d27853a3b8eb
gferraro <g.ferraro22@gmail.com>
2022-07-26 10:18:44 +1200

    fix cond

--- track/track.py
+++ track/track.py
@@ -905,7 +905,7 @@ class Track:
             tag
             for tag in track_tags
             if not tag.get("automatic", False)
-            and tag.get("confidence") > min_confidence
+            and tag.get("confidence") >= min_confidence
         ]
 
         if not track_tags:
