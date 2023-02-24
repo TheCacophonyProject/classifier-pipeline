47b5d7a1688679d4d844635dd0df6a4ce9bc1126
cam <cameron.ryan.pears@gmail.com>
2023-02-24 20:33:09 +1300

    Fix pi requirements and update virtualenv path

--- piclassifier/run-classifier.sh
+++ piclassifier/run-classifier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/intel/openvino_2022/setupvars.sh
-/home/pi/.venv/classifier/bin/python3 /home/pi/classifier-pipeline/piclassify.py
+/home/pi/classifier/bin/python3 /home/pi/classifier-pipeline/piclassify.py
