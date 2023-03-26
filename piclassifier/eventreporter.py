9ce48bb07b54a40938ea4bad7a9bced281935816
gferraro <g.ferraro22@gmail.com>
2023-03-27 09:57:47 +1300

    fix dbus problems

--- piclassifier/eventreporter.py
+++ piclassifier/eventreporter.py
@@ -2,13 +2,14 @@ import time
 import dbus
 import logging
 import json
+from dbus.mainloop.glib import DBusGMainLoop
 
 DBUS_NAME = "org.cacophony.Events"
 DBUS_PATH = "/org/cacophony/Events"
 
 
 def throttled_event():
-    bus = dbus.SystemBus()
+    bus = dbus.SystemBus(mainloop=DBusGMainLoop())
     try:
         proxy = bus.get_object(DBUS_NAME, DBUS_PATH)
         proxy.Add("{}", "throttle", dbus.Int64(time.time_ns()))
@@ -16,12 +17,18 @@ def throttled_event():
         logging.error("throttle dbus error ", exc_info=True)
 
 
-def log_event(event_type, details):
-    bus = dbus.SystemBus()
-    data = {"description": {"details": details}}
+def log_event(event_type, details=None):
+    bus = dbus.SystemBus(mainloop=DBusGMainLoop())
+    data = None
+    if details is not None:
+        data = {"description": {"details": details}}
+
     try:
         proxy = bus.get_object(DBUS_NAME, DBUS_PATH)
-        json_d = json.dumps(data)
+        if data is None:
+            json_d = ""
+        else:
+            json_d = json.dumps(data)
         proxy.Add(json_d, event_type, dbus.Int64(time.time_ns()))
     except:
         logging.error("log event dbus error ", exc_info=True)
