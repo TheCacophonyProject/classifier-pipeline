d8c664ae01247c99674b75df1434e931f82f8e51
gferraro <g.ferraro22@gmail.com>
2023-07-04 14:23:23 +1200

    remove test creds

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 hub_repo="cacophonyproject/classifier"
-TRAVIS_TAG="0.1"
-DOCKER_USERNAME="giampaolof"
-DOCKER_PASSWORD="%8!%*scU7sVe_;^"
 docker build -t $hub_repo .
 
 docker tag $hub_repo $hub_repo:$TRAVIS_TAG
