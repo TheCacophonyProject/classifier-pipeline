1c90bc874235c6a24e55c0712cf4e9e52101aac4
gferraro <g.ferraro22@gmail.com>
2023-08-30 14:13:53 +1200

    check bin

--- build.py
+++ build.py
@@ -315,7 +315,8 @@ def split_label(dataset, label, existing_test_count=0, max_samples=None):
             # sample.camera = "{}-{}".format(sample.camera, camera_type)
             add_to.add_sample(sample)
             if label in dontsplit and sample.bin_id in dataset.samples_by_bin:
-                dataset.samples_by_bin[sample.bin_id].remove(sample)
+                if sample in dataset.samples_by_bin[sample.bin_id]:
+                    dataset.samples_by_bin[sample.bin_id].remove(sample)
         samples_by_bin[sample_bin] = []
         last_index = i
         track_count = len(tracks)
@@ -343,8 +344,7 @@ def split_label(dataset, label, existing_test_count=0, max_samples=None):
             # sample.camera = "{}-{}".format(sample.camera, camera_type)
             train_c.add_sample(sample)
             added += 1
-
-            if label in dontsplit and sample.bin_id in dataset.samples_by_bin:
+            if sample in dataset.samples_by_bin[sample.bin_id]:
                 dataset.samples_by_bin[sample.bin_id].remove(sample)
 
         samples_by_bin[sample_bin] = []
