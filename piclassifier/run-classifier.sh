fb17c67d8f923f0845d5758b0360c46e42e50bb4
gferraro <g.ferraro22@gmail.com>
2023-09-13 11:18:43 +1200

    use pi classifier yaml

--- piclassifier/run-classifier.sh
+++ piclassifier/run-classifier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/intel/openvino_2022/setupvars.sh
-/home/pi/classifier/bin/python3 /home/pi/classifier-pipeline/piclassify.py
+/home/pi/classifier/bin/python3 /home/pi/classifier-pipeline/piclassify.py -c /home/pi/classifier-pipeline/pi-classifier.yaml
