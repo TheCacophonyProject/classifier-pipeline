96aa5c6971d36679f046b6b8c18b51aeb9beffe3
gferraro <g.ferraro22@gmail.com>
2023-11-01 17:34:49 +1300

    update script

--- pypi_push.sh
+++ pypi_push.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-python3 -m build
-
 set -xe
-TAG=$TRAVIS_TAG
-
-sed -i "s/NEWVERSIONHERE/${TAG}/g" pyproject.toml
-
-hub_repo="cacophonyproject/classifier"
-docker build -t $hub_repo . -f docker/DockerFile
-
-docker tag $hub_repo $hub_repo:$TRAVIS_TAG
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $hub_repo:$TRAVIS_TAG
+python3 -m build
+python3 -m twine upload  dist/*
