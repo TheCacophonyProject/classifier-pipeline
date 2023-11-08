b2eade1bcfc5790bbef25999f9cd39f5c0bfa0cb
gferraro <g.ferraro22@gmail.com>
2023-11-08 14:43:21 +1300

    push latest tag name, download latest model script

--- docker_push.sh
+++ docker_push.sh
@@ -7,3 +7,4 @@ docker tag $hub_repo $hub_repo:$TRAVIS_TAG
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $hub_repo:$TRAVIS_TAG
+docker push $hub_repo:latest
