e227d11968d903f3742de52bfeb1b264f5e0b49f
gferraro <g.ferraro22@gmail.com>
2024-01-09 19:01:17 +1300

    update auto build

--- src/autobuild.sh
+++ src/autobuild.sh
@@ -3,9 +3,8 @@
 set -e
 set -x
 config="classifier-thermal.yaml"
-month_ago=$(python3 rebuildDate.py -c $config)
-echo $month_ago
-python3 ../../cptv-download/cptv-download.py -l 0 -i 'poor tracking' -i 'untagged' -i 'part' -i 'untagged-by-humans' -i 'unknown' -i 'unidentified' -m 'human-tagged' --start-date "$month_ago" "../clips$month_ago" useremail@email.com userpassword
-echo "Downloading into ../clips$month_ago"
-python3 load.py -target "../clips$month_ago"  -c $config
-python3 build.py -c $config
+# month_ago=$(python3 rebuildDate.py -c $config)
+# echo $month_ago
+python3 ../../cptv-download/cptv-download.py -l 0 -i 'poor tracking' -i 'untagged' -i 'part' -i 'untagged-by-humans' -i 'unknown' -i 'unidentified' -m 'human-tagged' "/data2/cptv-files" useremail@email.com userpassword
+echo "Downloading into /data2/cptv-files
+python3 build.py -c $config --ext ".cptv" /data2/cptv-files
