1cf33873bf6efc87ecb62f58bbf4b49008c0513e
gferraro <g.ferraro22@gmail.com>
2024-06-17 16:58:05 +1200

    update travis

--- pypi_push.sh
+++ pypi_push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -xe
-hatch  build -t wheel
-python3 -m build -w -C="--global-option=--plat-name" -C="--global-option=manylinux_2_28_aarch64.whl"
+python3 -m build 
 python3 -m twine upload  dist/*
