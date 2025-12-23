242f986d1a48f5612571bb36d3d483314c4c71ab
gferraro <g.ferraro22@gmail.com>
2025-12-23 14:36:20 +1300

    auto build stuff

--- src/autobuild.sh
+++ src/autobuild.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 set -x
-conda init bash
-conda activate tf
-config="classifier-thermal.yaml"
-echo "Saving into $1"
+config=$2
+echo "Saving into $1 with config $2"
 month_ago=$(python3 rebuildDate.py $1)
 echo $month_ago
-python3 ../../cptv-download/cptv-download.py -l 0 -i 'poor tracking' -i 'untagged' -i 'part' -i 'untagged-by-humans' -i 'unknown' -i 'unidentified' -m 'human-tagged' --start-date "$month_ago" "$1" useremail@email.com userpassword
-echo "Downloading into $1"
-python3 build.py -c $config --ext ".cptv" $1
-dt=$(date '+%d%m%Y-%H%M%S');
-export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/cp/miniconda3/envs/tf/lib/
-python3 train.py -c $config $dt 
+cd ../../cptv-download
+db_file=$(python3 backup-download.py ./dbs)
+sudo -u postgres pg_restore --clean -d cacodb $db_file
+cd ../../classifier-pipeline/src
+#python3 ../../cptv-download/cptv-download-direct.py --start-date "$month_ago" "$1"
+#echo "Downloading into $1"
+#python3 build.py -c $config --ext ".cptv" $1
+#dt=$(date '+%d%m%Y-%H%M%S');
+#export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/cp/miniconda3/envs/tf/lib/
+#python3 train.py -c $config $dt 
