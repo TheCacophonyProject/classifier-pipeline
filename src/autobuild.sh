0fa4a1e1c4b531e6a75108203f0a5bb287a0bdf4
gferraro <g.ferraro22@gmail.com>
2026-01-06 14:03:43 +1300

    adjust autobuild

--- src/autobuild.sh
+++ src/autobuild.sh
@@ -9,9 +9,8 @@ cd ../../cptv-download
 db_file=$(python3 backup-download.py ./dbs)
 sudo -u postgres pg_restore --clean -d cacodb $db_file
 cd ../../classifier-pipeline/src
-#python3 ../../cptv-download/cptv-download-direct.py --start-date "$month_ago" "$1"
-#echo "Downloading into $1"
-#python3 build.py -c $config --ext ".cptv" $1
-#dt=$(date '+%d%m%Y-%H%M%S');
-#export XLA_FLAGS=--xla_gpu_cuda_data_dir=/home/cp/miniconda3/envs/tf/lib/
-#python3 train.py -c $config $dt 
+python3 ../../cptv-download/cptv-download-direct.py --start-date "$month_ago" "$1"
+echo "Downloading into $1"
+python3 build.py -c $config --ext ".cptv" $1
+dt=$(date '+%d%m%Y-%H%M%S');
+python3 train.py -c $config $dt 
