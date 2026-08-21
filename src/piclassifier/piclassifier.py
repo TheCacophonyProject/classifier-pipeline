092d7dc492b00fa830b2a44da1d8115b45f0494c
GP <g.ferraro22@gmail.com>
2026-08-21 13:41:04 +1200

    fix tests

--- src/piclassifier/piclassifier.py
+++ src/piclassifier/piclassifier.py
@@ -597,7 +597,7 @@ class PiClassifier(Processor):
                 track_prediction, model_id = self.get_best_prediction(track.get_id())
                 if track_prediction is None:
                     continue
-                predicted_tags = track_prediction.predicted_tag(thresholds_per_label = self.predictions[model_id].thresholds_per_label,group_by_parents = False) 
+                predicted_tags = track_prediction.predicted_tags(thresholds_per_label = self.predictions[model_id].thresholds_per_label,group_by_parents = False) 
                 if  "false-positive" not in predicted_tags:
                     track_prediction.tracking = True
                     self.monitored_tracks[track.get_id()] = track
@@ -726,13 +726,13 @@ class PiClassifier(Processor):
                 pred, model_id = self.get_best_prediction(track.get_id())
                 if pred is not None and pred.max_score is not None:
                     confidence = round(100 * pred.max_score)
-                    tags = pred.predicted_tag(thresholds_per_label = self.predictions[model_id].thresholds_per_label, group_by_parents=False)
+                    tags = pred.predicted_tags(thresholds_per_label = self.predictions[model_id].thresholds_per_label, group_by_parents=False)
             regions = track.bounds_history
             if track.thumb_info is None:
                 track.thumb_info = ThumbInfo(track.get_id())
             track.thumb_info.predicted_confidence = confidence
             track.thumb_info.predicted_tags = tags
-            track.thumb_info.is_fp = "false-positive" in tags
+            track.thumb_info.is_fp = tags is not None and "false-positive" in tags
             i = len(regions) - 1
             first_loop = True
             # go reverse and break when reach already checked frame
