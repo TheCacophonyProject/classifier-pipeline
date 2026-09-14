314558ff4185a33b81bd2c2942de736f50f10606
GP <g.ferraro22@gmail.com>
2026-09-14 16:32:26 +1200

    set version

--- src/piclassify.py
+++ src/piclassify.py
@@ -3,12 +3,7 @@
 """
 Script to classify animals within a CPTV video file.
 """
-
-import multiprocessing
-
-multiprocessing.set_forkserver_preload(["numpy"])
 if __name__ == "__main__":
-    multiprocessing.set_start_method("forkserver")
     from piclassifier.piclassify import main
 
     main()
