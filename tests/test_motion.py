7ef476b4c2ed883754d1c8a33a39f399a53e8d16
GP <g.ferraro22@gmail.com>
2026-09-25 09:51:45 +1200

    fix motion test

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -57,7 +57,7 @@ def test_motion(
         )
 
         motion_detector = CPTVMotionDetector(
-            thermal_config, config.tracking.motion,config.tracking.motion.dynamic_thresh, headers
+            thermal_config, thermal_config.base_motion.use_defaults_for("lepton3.5"),config.tracking.motion.dynamic_thresh, headers
         )
         for i, frame in enumerate(reader):
             motion_detector.process_frame(frame)
